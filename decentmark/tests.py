a4361b623bffee06e21f3c05bf80f418fd900155
Prabhanjan Veeravilli <v.prabhanjan123@gmail.com>
2018-09-04 08:59:06 +0800

    Test case for unit creation
    Checking the unit name label and length.

--- decentmark/tests.py
+++ decentmark/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
 
-# Create your tests here.
+from decentmark.models import Unit
+import datetime
+from django.core.exceptions import ValidationError
+
+
+
+class UnitModelTest(TestCase):
+    def setUpTestData(cls):
+        Unit.objects.create(name='Python')
+
+    def test_name_label(self):
+        unit = Unit.objects.get()  # type: Unit
+        field_label = unit._meta.get_field('name').verbose_name
+        self.assertEquals(field_label, 'name')
+
+    def test_name_max_length(self):
+        unit = Unit.objects.get()
+        max_length = unit._meta.get_field('name').max_length
+        self.assertEquals(max_length, 200)
+
