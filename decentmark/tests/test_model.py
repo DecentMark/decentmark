c362826f6a5dc5383fedb0b7953aa395b7949815
Prabhanjan Veeravilli <v.prabhanjan123@gmail.com>
2018-09-25 10:33:02 +0800

    Model Test cases

--- decentmark/tests/test_model.py
+++ decentmark/tests/test_model.py
@@ -1,6 +1,6 @@
 from django.test import TestCase
 from decentmark.models import *
-# from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model
 # import datetime
 # from django.core.exceptions import ValidationError
 
@@ -33,31 +33,32 @@ class UnitModelTest(TestCase):
             unit.full_clean()
 
 
-# class UnitUsersTest(TestCase):
-#
-#     @classmethod
-#     def setUpTestData(cls):
-#         unit = Unit.objects.create(name='Python', start='2018-10-25 14:30:59', end='2017-10-25 14:30:59',
-#                                    description='111', deleted='False')
-#         user = get_user_model.create()
-#         UnitUsers.objects.create(unit=unit, user=user, create='False', mark='False', submit='True')
-#
-#     def test_str(self):
-#         unit_user = UnitUsers.objects.get(id=1)
-#         expected_object_name = str(unit_user.unit) + '-' + str(unit_user.user)
-#         self.assertEquals(expected_object_name, str(unit_user))
+class UnitUsersTest(TestCase):
 
-#     def test_verbose_name(self):
-#         unit_user = Unit.objects.get(id=1)
-#         field_label = unit_user._meta.get_field('Unit User').verbose_name
-#         self.assertEquals(field_label, 'Unit User')
-#
-#     def test_verbose_name_plural(self):
-#         unit_user = Unit.objects.get(id=1)
-#         field_label = unit_user._meta.get_field('Unit Users').verbose_name_plural
-#         self.assertEquals(field_label, 'Unit Users')
-#
-#
+    @classmethod
+    def setUpTestData(cls):
+        unit = Unit.objects.create(name='Python', start='2018-10-25 14:30:59', end='2017-10-25 14:30:59',
+                                   description='111', deleted='False')
+        user = get_user_model().objects.create_superuser(username='admin', email='admin@decent.mark',
+                                                         password='password')
+        UnitUsers.objects.create(unit=unit, user=user, create='False', mark='False', submit='True')
+
+    def test_str(self):
+        unit_user = UnitUsers.objects.get(id=1)
+        expected_object_name = str(unit_user.unit) + ' - ' + str(unit_user.user)
+        self.assertEquals(expected_object_name, str(unit_user))
+
+    # def test_verbose_name(self):
+    #     unit_user = Unit.objects.get(id=1)
+    #     field_label = unit_user._meta.get_field('Unit User').verbose_name
+    #     self.assertEquals(field_label, 'Unit User')
+    #
+    # def test_verbose_name_plural(self):
+    #     unit_user = Unit.objects.get(id=1)
+    #     field_label = unit_user._meta.get_field('Unit Users').verbose_name_plural
+    #     self.assertEquals(field_label, 'Unit Users')
+
+
 class AssignmentTest(TestCase):
 
     @classmethod
@@ -84,27 +85,27 @@ class AssignmentTest(TestCase):
         expected_object_name = str(assignment.unit) + " - " + str(assignment.name)
         self.assertEquals(expected_object_name, str(assignment))
 
-#
-# class SubmissionTest(TestCase):
-#
-#     @classmethod
-#     def setUpTestData(cls):
-#         user = settings.AUTH_USER_MODEL.objects.create_user(username='admin', email='admin@decent.mark',
-#                                                             password='password')
-#         unit = Unit.objects.create(name='Python', start='2018-10-25 14:30:59', end='2017-10-25 14:30:59',
-#                                    description='111', deleted='False')
-#         assignment = Assignment.objects.create(unit=unit, name='Python Lab 1', start='2018-10-28 14:30:59',
-#                                                end='2018-10-25 14:30:59', description='111', attempts=1, total=0,
-#                                                test='we', solution='Answer', template='Template', deleted='False')
-#         Submission.objects.create(assignment=assignment, user=user, date='2018-10-28 14:30:59', solution='Answer',
-#                                   marked='False', mark=10, feedback='Good')
-#
-#     def test_mark(self):
-#         mark = Submission(mark=0)
-#         with self.assertRaises(ValidationError):
-#             mark.full_clean()
-#
-#     def test_str(self):
-#         submission = Submission.objects.get(id=1)
-#         expected_object_name = str(submission.assignment) + '-' + str(submission.user)
-#         self.assertEquals(expected_object_name, str(submission))
+
+class SubmissionTest(TestCase):
+
+    @classmethod
+    def setUpTestData(cls):
+        user = get_user_model().objects.create_superuser(username='admin', email='admin@decent.mark',
+                                                         password='password')
+        unit = Unit.objects.create(name='Python', start='2018-10-25 14:30:59', end='2017-10-25 14:30:59',
+                                   description='111', deleted='False')
+        assignment = Assignment.objects.create(unit=unit, name='Python Lab 1', start='2018-10-28 14:30:59',
+                                               end='2018-10-25 14:30:59', description='111', attempts=1, total=0,
+                                               test='we', solution='Answer', template='Template', deleted='False')
+        Submission.objects.create(assignment=assignment, user=user, date='2018-10-28 14:30:59', solution='Answer',
+                                  marked='False', mark=10, feedback='Good')
+
+    def test_mark(self):
+        mark = Submission(mark=0)
+        with self.assertRaises(ValidationError):
+            mark.full_clean()
+
+    def test_str(self):
+        submission = Submission.objects.get(id=1)
+        expected_object_name = str(submission.assignment) + ' - ' + str(submission.user)
+        self.assertEquals(expected_object_name, str(submission))
